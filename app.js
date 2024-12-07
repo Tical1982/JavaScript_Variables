9975aebb14efa26ba2f8bd6e2e90f0ddc6c9155d
johnathan robinson <johnathan.l.robinson@gmail.com>
2024-12-07 09:27:15 -0600

    This is my school exercise

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+const firstName = "Johnathan";
+let lastName = "Robinson";
+var age = 41;
 
-// Exercise 2
+lastName="Robinson";
+ age = "41";
 
-// Exercise 3
+let language = "JavaScript";
+let createdYear = "1995";
+let isCaseSensitive = "true";
+
+console.log (`${language} was first released in ${createdYear}.`);
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = "true"; 
